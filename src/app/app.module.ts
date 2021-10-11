9b981e7611c6874e90c876605b049b7a26132fde
KamleshBahekar <kamleshbahekar027@gmail.com>
2021-10-11 12:15:55 +0530

    print

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
+import { NgxPrintModule } from 'ngx-print';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +33,8 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserAnimationsModule,
     MatSnackBarModule,
     MatButtonModule,
-    SheardModule
+    SheardModule,
+    NgxPrintModule
   ],
   exports: [
     MatSnackBarModule,
